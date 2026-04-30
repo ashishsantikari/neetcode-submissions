5563e64ba061edf2c5e7527fafdae8fad0030b82
Ashish Santikari <hi@ashishsantikari.info>
2026-04-30 21:42:43 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, str: str) -> bool:
+        stack = []
+        pairs = { "}": "{", "]" : "[", ")": "(" }
+        i = 1
+        stack.append(str[0])
+        while i < len(str):
+            c = str[i]
+            if len(stack) > 0 and c in pairs and pairs[c] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(c)
+            print("resulting stack", stack)
+            i += 1
+        if len(stack) > 0:
+            return False
+
+        return True
+    
